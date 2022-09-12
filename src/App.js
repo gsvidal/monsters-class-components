8f490cd90a1a655ff305f0cdbe683f67cf5d9467
Gonzo <gonzalovidal.dev@gmail.com>
2022-09-12 14:01:28 -0500

    Fetch data from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      monsters: [],
+    };
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((result) =>
+        this.setState(
+          () => {
+            console.log('before update state');
+            return { monsters: result };
+          },
+          () => {
+            console.log('after update state but with new state');
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log('render App component');
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.state.monsters.map((monster) => (
+            <h2 key={monster.name}>{monster.name}</h2>
+          ))}
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
